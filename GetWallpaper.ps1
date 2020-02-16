f46960ab625f0bad62de6998fffb4963f0d7e130
Hans Wustrack <hans2792@gmail.com>
2020-02-16 20:11:02 +1300

    clean up old files

--- GetWallpaper.ps1
+++ GetWallpaper.ps1
@@ -1,24 +1,27 @@
-# Secrets
+# secrets
 $creds = Get-StoredCredential -Target unsplash
 $access_key = $creds.GetNetworkCredential().password
 
-# Query parameters
+# query parameters
 $collections = "437035,3652377,8362253"
 
-# Construct Unsplash API request parameters
+# construct request parameters
 $url = "https://api.unsplash.com/photos/random"
 $headers = @{ "Accept-Version" = "v1"; Authorization = "Client-ID $access_key" }
 $params = @{ collections = $collections; featured = "true"; orientation = "landscape" }
 
-# Make API requests to get random photo
+# make API requests to Unsplash for a random photo
 $response = Invoke-WebRequest $url -Method Get -Headers $headers -Body $params
 $content = $response | ConvertFrom-Json
 $imgUrl = $content.urls.full
 $id = $content.id
 Invoke-WebRequest $imgUrl -OutFile ".\$id.jpg"
 
-# Write img metadata to file
+# write img metadata to file
 $sel = $content | 
     Select-Object @{n = "link"; e = { $_.links.html } }, @{n = "name"; e = { $_.user.name } }, 
         description, @{n = "location"; e = { $_.location.title } }, @{n = "retrieval time"; e = { Get-Date } }
-$sel | Format-List | Out-File -FilePath .\info.txt
+$sel | Format-List | Out-File -FilePath ".\$id.txt"
+
+# remove old files
+Get-ChildItem -Path .\* -Include ("*.jpg", "*.txt") | Where-Object {$_.CreationTime -lt (Get-Date).AddMinutes(-60)} | Remove-Item
